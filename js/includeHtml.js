9980c57bc53d1f3165ae4b280d3fbcb8c22aaee0
lubinga <pfatsch@icloud.com>
2024-01-11 17:54:11 +0100

    Update Board.html and added includeHTML Script

---
+++ js/includeHtml.js
@@ -0,0 +1,14 @@
+// Include HTML Script started on body onload
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
