44ab389ba624a5c8e2c525bf726ba23085cd4da6
lubinga <pfatsch@icloud.com>
2024-01-15 17:34:35 +0100

    Drag and Drop Logic implemented on the Board

--- js/board.js
+++ js/board.js
@@ -1,6 +1,25 @@
 window.onload = async () => {
     await includeHTML(); //Renders external templates and waits for it
-    let elem = document.getElementById('emptyTodo');
-    elem.innerHTML = 'There are plenty... go on and write some js';
     //Run functions which manipulate elements from rendered templates 
 };
+
+//Drag and Drop Logic
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    // implement logic to check if there are opther cards- else render placeholder No tasks to do
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    
+    targetId = document.getElementById(ev.target.id);
+    placeholder = targetId.querySelector('.todo-empty');    
+    placeholder.classList.add('d-none');
+
+    ev.target.appendChild(document.getElementById(data));
+}
