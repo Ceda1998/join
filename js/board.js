70f4cedff03ced4fe1aa6c1221b2d5a82fbe2632
lubinga <pfatsch@icloud.com>
2024-01-15 16:22:34 +0100

    Updated Board with Card design- still some bugfix tbd

--- js/board.js
+++ js/board.js
@@ -1,6 +1,6 @@
 window.onload = async () => {
-    await includeHTML(); //Renders external templates
+    await includeHTML(); //Renders external templates and waits for it
     let elem = document.getElementById('emptyTodo');
     elem.innerHTML = 'There are plenty... go on and write some js';
-    console.log(elem);
+    //Run functions which manipulate elements from rendered templates 
 };
