be9c5f433771da51e5f1afe07067b97e60f66c89
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-01-19 20:13:42 +0100

    added functions to addTask

--- js/addTask.js
+++ js/addTask.js
@@ -1,5 +1,20 @@
 let isButtonToggled = [false, false, false];
 let isArrowRotated = false;
+let subtasks = [];
+
+/* JSON-ARRAY */
+
+let task = [
+    {
+        'title': 'Hallo',
+        'description': 'Wie geht es?',
+        'assignedTo': 'Anna',
+        'dueDate': '01/01/2024',
+        'prio': 'urgent',
+        'category': 'User Story',
+        'subtasks': 'Do this'
+    }
+]
 
 function setPrio(num) {
     const TOTAL_BUTTONS = 3;
@@ -35,4 +50,55 @@ function selectCategory(num) {
     let input = document.getElementById('categoryInput');
     let selectedCategory = document.getElementById(`category${num}`).innerHTML;
     input.value = selectedCategory;
+    toggleCategoryDropDown();
+}
+
+
+function addSubtask() {
+    let input = document.getElementById('subtaskInput');
+    let subtask = input.value;
+    subtasks.push(subtask);
+    renderSubtasks();
+    input.value = '';
+}
+
+
+function renderSubtasks() {
+    let subtasksContainer = document.getElementById('addedSubtasksContainer');
+    subtasksContainer.innerHTML = '';
+    for (let i = 0; i < subtasks.length; i++) {
+        const subtask = subtasks[i];
+        subtasksContainer.innerHTML += /*html*/`
+            <div class="added-subtask-container">
+                <input class="added-subtask" type="text" value='• ${subtask}'>
+                <div class="tool-container">
+                    <img src="./assets/img/edit.png" class="edit-img">
+                    <div class="tool-separator"></div>
+                    <img src="./assets/img/delete-img.png" class="delete-img">
+                </div>
+            </div>
+        `;
+    }
+}
+
+
+function toggleAssignedToDropDown() {
+    let assignedToDropDown = document.getElementById('assignedToDropDown');
+    let arrowAssignedTo = document.getElementById('arrowAssignedTo');
+    assignedToDropDown.classList.toggle('d-none');
+    isArrowRotated = !isArrowRotated;
+    arrowAssignedTo.style.transform = isArrowRotated ? 'rotate(180deg)' : '';
+}
+
+
+function selectPerson(num) {
+    let input = document.getElementById('assignedToInput');
+    let selectedPerson = document.getElementById(`person${num}`).innerHTML;
+    input.value = selectedPerson;
+    toggleAssignedToDropDown();
+}
+
+
+function createTask() {
+    
 }
