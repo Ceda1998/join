f57c7d7b15b1c5c039869d35489b1b284af05d2e
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-01-19 16:33:48 +0100

    added function setPrio

--- js/addTask.js
+++ js/addTask.js
@@ -1,4 +1,21 @@
-function addPrio() {
-    let prio = document.getElementById('urgent');
-    prio.classList.add('urgent');
+let isButtonToggled = [false, false, false];
+
+function setPrio(num) {
+    const TOTAL_BUTTONS = 3;
+
+    for (let i = 0; i < TOTAL_BUTTONS; i++) {
+        if (i + 1 == num || isButtonToggled[i] === true) {
+            isButtonToggled[i] = !isButtonToggled[i]
+            const selectedButton = document.getElementById(`prioButton${i+1}`);
+            const selectedImgPrioColor = document.getElementById(`prioColor${i+1}`);
+            const selectedImgPrioWhite = document.getElementById(`prioWhite${i+1}`);
+            const selectedPrio = document.getElementById(`prio${i+1}`).innerHTML.toLowerCase();
+        
+            selectedButton.classList.toggle(`${selectedPrio}`);
+            selectedButton.classList.toggle('prioTextWhite');
+            selectedImgPrioColor.classList.toggle('d-none');
+            selectedImgPrioWhite.classList.toggle('d-none');
+        }        
+    }
+
 }
