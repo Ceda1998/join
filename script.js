0a18ac216f7421e45da0823c1e83a0355f7597ef
Cedric Daunke <daunke40@gmail.com>
2024-01-25 08:47:40 +0100

    finished legal sites design and functions

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const GUEST_USER = "GU";
 function openHelpMenu() {
     window.location.href = "help.html";
 }
+
+function returnToSummary() {
+    window.location.href = "../summary.html";
+}
+
+function returnToLogin() {
+    window.location.href = "../index.html";
+}
