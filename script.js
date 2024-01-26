cc66e51bd4931d6be37634f37ddfda51946fe229
Cedric Daunke <daunke40@gmail.com>
2024-01-26 08:26:51 +0100

    more responsive

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function returnToSummary() {
 function returnToLogin() {
     window.location.href = "../index.html";
 }
+
+function returnToBoard() {
+    window.location.href = "../board.html";
+}
+
+function returnTasks() {
+    window.location.href = "../add_task.html";
+}
+
+function returnContacts() {
+    window.location.href = "../contacts.html";
+}
