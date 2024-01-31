4810e7cdaf5f83ebfa69dfce49267c490662121b
Cedric Daunke <daunke40@gmail.com>
2024-01-31 17:55:47 +0100

    implemented logout on all sites

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ function returnTasks() {
 function returnContacts() {
     window.location.href = "../contacts.html";
 }
+
+function toggleLogOutMenu() {
+    let logoutMenu = document.getElementById('logoutContent');
+    if (logoutMenu.style.display === 'none' || logoutMenu.style.display === '') {
+        logoutMenu.style.display = 'block';
+    } else {
+        logoutMenu.style.display = 'none';
+    }
+}
